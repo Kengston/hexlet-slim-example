0d48a92d4395597122d6cba83327d2366e4b7962
Kengston <danidc@mail.ru>
2023-10-11 20:41:04 +0400

    Search added

--- public/index.php
+++ public/index.php
@@ -5,11 +5,10 @@ require __DIR__ . '/../vendor/autoload.php';
 use Slim\Factory\AppFactory;
 use DI\Container;
 use Illuminate\Support\Collection;
-
-$users = ['mike', 'mishel', 'adel', 'keks', 'kamila'];
+use Ramsey\Uuid\Uuid;
 
 $container = new Container();
-$container -> set('renderer', function () {
+$container->set('renderer', function () {
     return new \Slim\Views\PhpRenderer(__DIR__ . '/../templates');
 });
 
@@ -21,23 +20,14 @@ $app->get('/', function ($request, $response) {
     return $response;
 });
 
-
-$app->get('/courses/{id}', function ($request, $response, array $args) {
-    $id = $args['id'];
-    return $response->write("Course id: {$id}");
-});
-
-$app->get('/users/{id}', function ($request, $response, $args) {
-    $params = ['id' => $args['id'], 'nickname' => 'user-' . $args['id']];
-
-    return $this->get('renderer')->render($response, 'users/show.phtml', $params);
-});
-
-$app->get('/users', function ($request, $response) use ($users) {
+$app->get('/users', function ($request, $response) {
     $searchTerm = $request->getQueryParams()['search'] ?? '';
 
+    $userJson = file_get_contents('users.json');
+    $users = json_decode($userJson, true);
+
     $filteredUsers = collect($users)->filter(function ($user) use ($searchTerm) {
-        return str_starts_with($user, $searchTerm);
+        return is_array($user) && array_key_exists('name', $user) && str_starts_with($user['name'], $searchTerm);
     });
 
     $params = ['users' => $filteredUsers, 'searchTerm' => $searchTerm];
@@ -45,8 +35,63 @@ $app->get('/users', function ($request, $response) use ($users) {
     return $this->get('renderer')->render($response, 'users/index.phtml', $params);
 });
 
+$app->get('/users/new', function ($request, $response) {
+    $params = [
+        'user' => ['name' => '', 'email' => ''],
+        'errors' => []
+    ];
+
+    return $this->get('renderer')->render($response, 'users/new.phtml', $params);
+});
+
 $app->post('/users', function ($request, $response) {
-    return $response->withStatus(302);
+    $userData = $request->getParsedBody()['user'];
+    $errors = [];
+
+    if (empty($userData['name'])) {
+        $errors['name'] = 'Nickname is empty';
+    }
+
+    if (empty($userData['email']) || !filter_var($userData['email'], FILTER_VALIDATE_EMAIL)) {
+        $errors['email'] = 'Invalid email address';
+    }
+
+    if (empty($errors)) {
+        $user = [
+            'name' => $userData['name'],
+            'email' => $userData['email']
+        ];
+
+        $userJson = file_get_contents('users.json');
+        $users = json_decode($userJson, true);
+        $users[] = $user;
+
+        $json = json_encode($users, JSON_PRETTY_PRINT);
+        file_put_contents('users.json', $json);
+
+        return $response->withHeader('Location', '/users/' . $user['name'])->withStatus(302);
+    } else {
+        $params = [
+            'user' => $userData,
+            'errors' => $errors
+        ];
+
+        return $this->get('renderer')->render($response, 'users/new.phtml', $params);
+    }
+});
+
+$app->get('/users/{name}', function ($request, $response, $args) {
+    $name = $args['name'];
+    $userJson = file_get_contents('users.json');
+    $users = json_decode($userJson, true);
+
+    foreach ($users as $user) {
+        if (isset($user['name']) && $user['name'] === $name) {
+            return $this->get('renderer')->render($response, 'users/show.phtml', ['user' => $user]);
+        }
+    }
+
+    return $response->write('User not found')->withStatus(404);
 });
 
 
