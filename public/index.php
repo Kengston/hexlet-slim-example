72bd93a7ecb63b2d3cbd1217c715835ff488d32e
Kengston <danildc@mail.ru>
2023-10-17 17:17:37 +0400

    User edit added

--- public/index.php
+++ public/index.php
@@ -1,106 +1,163 @@
-<?php
+    <?php
 
-require __DIR__ . '/../vendor/autoload.php';
+    require __DIR__ . '/../vendor/autoload.php';
 
-use Slim\Factory\AppFactory;
-use Slim\Routing\RouteContext;
-use DI\Container;
-use Illuminate\Support\Collection;
-use Ramsey\Uuid\Uuid;
+    use Slim\Factory\AppFactory;
+    use Slim\Middleware\MethodOverrideMiddleware;
+    use DI\Container;
+    use Illuminate\Support\Collection;
 
-$container = new Container();
-$container->set('renderer', function () {
-    return new \Slim\Views\PhpRenderer(__DIR__ . '/../templates');
-});
+    session_start();
 
-$app = AppFactory::createFromContainer($container);
-$app->addErrorMiddleware(true, true, true);
+    $container = new Container();
 
-$container->set('router', function () use ($app) {
-    return $app->getRouteCollector()->getRouteParser();
-});
+    AppFactory::setContainer($container);
+    $app = AppFactory::create();
 
-$app->get('/', function ($request, $response) {
-    $response->getBody()->write('Welcome To Slim!');
-    return $response;
-})->setName('home');
+    $app->add(MethodOverrideMiddleware::class);
+    $app->addErrorMiddleware(true, true, true);
 
-$app->group('/users', function($app) {
-    $app->get('', function ($request, $response) {
-        $searchTerm = $request->getQueryParams()['search'] ?? '';
+    $container->set('renderer', function () {
+        return new \Slim\Views\PhpRenderer(__DIR__ . '/../templates');
+    });
 
-        $userJson = file_get_contents('users.json');
-        $users = json_decode($userJson, true);
+    $container->set('router', function () use ($app) {
+        return $app->getRouteCollector()->getRouteParser();
+    });
 
-        $filteredUsers = collect($users)->filter(function ($user) use ($searchTerm) {
-            return is_array($user) && array_key_exists('name', $user) && str_starts_with($user['name'], $searchTerm);
-        });
+    $container->set('flash', function () {
+        return new \Slim\Flash\Messages();
+    });
 
-        $params = ['users' => $filteredUsers, 'searchTerm' => $searchTerm];
+    $app->get('/', function ($request, $response) {
+        $response->getBody()->write('Welcome To Slim!');
+        return $response;
+    })->setName('home');
 
-        return $this->get('renderer')->render($response, 'users/index.phtml', $params);
-    })->setName('users');
-
-    $app->get('/new', function ($request, $response) {
-        $params = [
-            'user' => ['name' => '', 'email' => ''],
-            'errors' => []
-        ];
-
-        return $this->get('renderer')->render($response, 'users/new.phtml', $params);
-    })->setName('new_user');
-
-    $app->post('', function ($request, $response) {
-        $userData = $request->getParsedBody()['user'];
-        $errors = [];
-
-        if (empty($userData['name'])) {
-            $errors['name'] = 'Nickname is empty';
-        }
-
-        if (empty($userData['email']) || !filter_var($userData['email'], FILTER_VALIDATE_EMAIL)) {
-            $errors['email'] = 'Invalid email address';
-        }
-
-        if (empty($errors)) {
-            $user = [
-                'name' => $userData['name'],
-                'email' => $userData['email']
-            ];
+    $app->group('/users', function($app) {
+        $app->get('', function ($request, $response) {
+            $searchTerm = $request->getQueryParams()['search'] ?? '';
 
             $userJson = file_get_contents('users.json');
             $users = json_decode($userJson, true);
-            $users[] = $user;
+
+            $filteredUsers = collect($users)->filter(function ($user) use ($searchTerm) {
+                return is_array($user) && array_key_exists('name', $user) && str_starts_with($user['name'], $searchTerm);
+            });
+
+            $params = ['users' => $filteredUsers, 'searchTerm' => $searchTerm];
+
+            return $this->get('renderer')->render($response, 'users/index.phtml', $params);
+        })->setName('users');
+
+        $app->get('/new', function ($request, $response) {
+            $params = [
+                'user' => ['name' => '', 'email' => ''],
+                'errors' => []
+            ];
+
+            return $this->get('renderer')->render($response, 'users/new.phtml', $params);
+        })->setName('new_user');
+
+        $app->post('', function ($request, $response) {
+            $userData = $request->getParsedBody()['user'];
+            $errors = [];
+
+            if (empty($userData['name'])) {
+                $errors['name'] = 'Nickname is empty';
+            }
+
+            if (empty($userData['email']) || !filter_var($userData['email'], FILTER_VALIDATE_EMAIL)) {
+                $errors['email'] = 'Invalid email address';
+            }
+
+            if (empty($errors)) {
+                $user = [
+                    'name' => $userData['name'],
+                    'email' => $userData['email']
+                ];
+
+                $userJson = file_get_contents('users.json');
+                $users = json_decode($userJson, true);
+                $users[] = $user;
+
+                $json = json_encode($users, JSON_PRETTY_PRINT);
+                file_put_contents('users.json', $json);
+
+                $flash = $this->get('flash')->addMessage('success', 'User was added successfully');
+
+                $url = $this->get('router')->urlFor('show_user', ['name' => $user['name']]);
+
+                return $response->withHeader('Location', $url)->withStatus(302);
+            } else {
+                $params = [
+                    'user' => $userData,
+                    'errors' => $errors
+                ];
+
+                return $this->get('renderer')->render($response, 'users/new.phtml', $params);
+            }
+        })->setName('create_user');
+
+        $app->get('/{name}', function ($request, $response, $args) {
+            $name = $args['name'];
+            $userJson = file_get_contents('users.json');
+            $users = json_decode($userJson, true);
+            $messages = $this->get('flash')->getMessages();
+
+            foreach ($users as $user) {
+                if (isset($user['name']) && $user['name'] === $name) {
+                    return $this->get('renderer')->render($response, 'users/show.phtml', ['user' => $user, 'messages' => $messages]);
+                }
+            }
+
+            return $response->write('User not found')->withStatus(404);
+        })->setName('show_user');
+
+        $app->get('/{name}/edit', function ($request, $response, array $args) {
+            $name = $args['name'];
+            $userJson = file_get_contents('users.json');
+            $users = json_decode($userJson, true);
+
+            $foundUser = collect($users)->first(function ($user) use ($name) {
+                return isset($user['name']) && $user['name'] === $name;
+            });
+
+            if ($foundUser === null) {
+                return $response->write('User not found')->withStatus(404);
+            }
+
+            $params = [
+                'user' => $foundUser,
+                'errors' => []
+            ];
+
+            return $this->get('renderer')->render($response, 'users/edit.phtml', $params);
+        })->setName('edit_user');
+
+        $app->patch('/{name}', function ($request, $response, $args) {
+            $name = $args['name'];
+            $userJson = file_get_contents('users.json');
+            $users = json_decode($userJson, true);
+
+            $data = $request->getParsedBodyParam('user');
+
+            foreach ($users as &$user) {
+                if (isset($user['name']) && $user['name'] === $name) {
+                    $user['name'] = $data['name'];
+                    break;
+                }
+            }
 
             $json = json_encode($users, JSON_PRETTY_PRINT);
             file_put_contents('users.json', $json);
 
-            $url = $this->get('router')->urlFor('show_user', ['name' => $user['name']]);
+            $flash = $this->get('flash')->addMessage('success', 'User was updated successfully');
+
+            $url = $this->get('router')->urlFor('show_user', ['name' => $data['name']]);
 
             return $response->withHeader('Location', $url)->withStatus(302);
-        } else {
-            $params = [
-                'user' => $userData,
-                'errors' => $errors
-            ];
+        });
+    });
 
-            return $this->get('renderer')->render($response, 'users/new.phtml', $params);
-        }
-    })->setName('create_user');
-
-    $app->get('/{name}', function ($request, $response, $args) {
-        $name = $args['name'];
-        $userJson = file_get_contents('users.json');
-        $users = json_decode($userJson, true);
-
-        foreach ($users as $user) {
-            if (isset($user['name']) && $user['name'] === $name) {
-                return $this->get('renderer')->render($response, 'users/show.phtml', ['user' => $user]);
-            }
-        }
-
-        return $response->write('User not found')->withStatus(404);
-    })->setName('show_user');
-});
-
-$app->run();
+    $app->run();
